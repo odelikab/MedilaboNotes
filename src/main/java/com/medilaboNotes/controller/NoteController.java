c710b0714fbd6801c00a7e9bad91c620fe003d88
odeli <misterkb5@hotmail.com>
2024-02-20 01:41:03 +0100

    note service

---
+++ src/main/java/com/medilaboNotes/controller/NoteController.java
@@ -0,0 +1,57 @@
+package com.medilaboNotes.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.medilaboNotes.model.Note;
+import com.medilaboNotes.services.NoteService;
+
+@RestController
+@RequestMapping("/notes")
+public class NoteController {
+
+	private static final Logger logger = LoggerFactory.getLogger(NoteController.class);
+
+	@Autowired
+	private NoteService noteService;
+
+	@GetMapping("/findAllNotes")
+	public List<Note> getNotes() {
+		return noteService.findAllNotes();
+	}
+
+	@PostMapping("/addNote")
+	public void saveNote(@RequestBody Note note) {
+		noteService.saveNote(note);
+	}
+
+	@GetMapping("/{id}")
+	public List<Note> getBook(@PathVariable Integer id) {
+		return noteService.findNoteById(id);
+	}
+
+	@GetMapping("/custom")
+	public List<String> getPatientNotesContaining(String patient) {
+
+		return noteService.getPatientNotesContaining(patient);
+	}
+
+	@GetMapping("/patient")
+	public Object getPatientGroups() {
+		return noteService.getPatientGroups();
+	}
+
+	@GetMapping("/notes")
+	public Object getPatientNotes() {
+		return noteService.getPatientNotes();
+	}
+}
---
+++ src/main/java/com/medilaboNotes/services/NoteService.java
@@ -0,0 +1,61 @@
+package com.medilaboNotes.services;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.medilaboNotes.model.Note;
+import com.medilaboNotes.repositories.NoteRepository;
+
+@Service
+public class NoteService {
+
+	@Autowired
+	private NoteRepository noteRepository;
+
+	public List<Note> findAllNotes() {
+		return noteRepository.findAll();
+	}
+
+	public Note saveNote(Note note) {
+		return noteRepository.insert(note);
+	}
+
+	public List<Note> findNoteById(Integer id) {
+		return noteRepository.findByPatId(id);
+	}
+
+	public List<Object> getPatientGroups() {
+		return noteRepository.groupByPatient();
+	}
+
+	public Object getPatientNotes() {
+		return noteRepository.findAllNotes();
+	}
+
+	public List<Note> findPatientAndNote(String patient, String symp) {
+		return noteRepository.findPatientAndNote(patient, symp);
+	}
+
+	public List<String> getPatientNotesContaining(String patient) {
+		String symptomes[] = new String[] { "Hémoglobine", "Microalbumine", "Taille", "Poids", "Fumeu", "Anormal",
+				"Cholestérol", "Vertige", "Rechute", "Réaction", "Anticorps" };
+		List<String> list = Arrays.asList(symptomes);
+		List<String> count = new ArrayList<>();
+
+		list.forEach(symp -> {
+			List<Note> listSymp = noteRepository.findPatientAndNote(patient, symp);
+			if (listSymp.size() > 0) {
+				count.add(symp);
+			}
+		});
+
+		String decl = "fume";
+		System.out.println(count);
+		return count;
+	}
+
+}
